03b0b93a36d8e79aa14a5b8e68c4c8d790b3c0c4
Baptiste Langlade <baptouuuu@gmail.com>
2017-05-21 13:23:55 +0200

    add missing intersection test

---
+++ tests/DefinitionSet/IntersectionTest.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Math\DefinitionSet;
+
+use Innmind\Math\{
+    DefinitionSet\Intersection,
+    DefinitionSet\Union,
+    DefinitionSet\SetInterface,
+    DefinitionSet\Set,
+    Algebra\Integer,
+    Algebra\Number
+};
+use PHPUnit\Framework\TestCase;
+
+class IntersectionTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            SetInterface::class,
+            new Intersection(
+                $this->createMock(SetInterface::class),
+                $this->createMock(SetInterface::class)
+            )
+        );
+    }
+
+    public function testStringCast()
+    {
+        $this->assertSame('∅∩∅', (string) new Intersection(new Set, new Set));
+    }
+
+    public function testContains()
+    {
+        $union = new Intersection(
+            new Set(
+                new Integer(1),
+                new Integer(2)
+            ),
+            new Set(
+                new Integer(4),
+                new Integer(5)
+            )
+        );
+
+        $this->assertFalse($union->contains(new Number(1)));
+        $this->assertFalse($union->contains(new Number(2)));
+        $this->assertFalse($union->contains(new Number(4)));
+        $this->assertFalse($union->contains(new Number(5)));
+        $this->assertFalse($union->contains(new Number(6)));
+        $this->assertFalse($union->contains(new Number(3)));
+        $this->assertFalse($union->contains(new Number(0)));
+    }
+
+    public function testUnion()
+    {
+        $union = (new Intersection(new Set, new Set))->union(new Set);
+
+        $this->assertInstanceOf(Union::class, $union);
+        $this->assertSame('∅∩∅∪∅', (string) $union);
+    }
+
+    public function testIntersect()
+    {
+        $intersection = (new Intersection(new Set, new Set))->intersect(new Set);
+
+        $this->assertInstanceOf(Intersection::class, $intersection);
+        $this->assertSame('∅∩∅∩∅', (string) $intersection);
+    }
+}
