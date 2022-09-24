d988ebad231ecca4a5404d632ada1c2f00c199cb
Baptiste Langlade <baptouuuu@gmail.com>
2022-09-24 17:52:02 +0200

    add asc and desc functions

--- src/functions.php
+++ src/functions.php
@@ -10,19 +10,41 @@ use Innmind\Math\{
 use Innmind\Immutable\Sequence;
 
 /**
+ * @psalm-pure
+ *
+ * @return -1|0|1
+ */
+function asc(Number $a, Number $b): int
+{
+    if ($a->equals($b)) {
+        return 0;
+    }
+
+    return $a->higherThan($b) ? 1 : -1;
+}
+
+/**
+ * @psalm-pure
+ *
+ * @return -1|0|1
+ */
+function desc(Number $a, Number $b): int
+{
+    if ($a->equals($b)) {
+        return 0;
+    }
+
+    return $b->higherThan($a) ? 1 : -1;
+}
+
+/**
  * @no-named-arguments
  * @psalm-pure
  */
 function max(Number $first, Number ...$numbers): Number
 {
     return Sequence::of($first, ...$numbers)
-        ->sort(static function(Number $a, Number $b): int {
-            if ($a->equals($b)) {
-                return 0;
-            }
-
-            return $b->higherThan($a) ? 1 : -1;
-        })
+        ->sort(desc(...))
         ->first()
         ->match(
             static fn($max) => $max,
@@ -37,13 +59,7 @@ function max(Number $first, Number ...$numbers): Number
 function min(Number $first, Number ...$numbers): Number
 {
     return Sequence::of($first, ...$numbers)
-        ->sort(static function(Number $a, Number $b): int {
-            if ($a->equals($b)) {
-                return 0;
-            }
-
-            return $a->higherThan($b) ? 1 : -1;
-        })
+        ->sort(asc(...))
         ->first()
         ->match(
             static fn($min) => $min,
