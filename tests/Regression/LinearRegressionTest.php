5e105ec7491ab6785fa10153e125050c706a82e7
Baptiste Langlade <baptouuuu@gmail.com>
2022-09-25 10:38:38 +0200

    Revert "use explicit abscissas"
    This reverts commit 250e61fe88633e2b239614a035469e065ef3487c.

--- tests/Regression/LinearRegressionTest.php
+++ tests/Regression/LinearRegressionTest.php
@@ -16,34 +16,29 @@ class LinearRegressionTest extends TestCase
 {
     public function testIntercept()
     {
-        $r = LinearRegression::of(Dataset::of([[0, 0], [1, 1]]));
+        $r = LinearRegression::of(Dataset::of([0, 1]));
         $this->assertInstanceOf(Number::class, $r->intercept());
         $this->assertSame($r->intercept(), $r->intercept());
         $this->assertSame(0, $r->intercept()->value());
 
-        $r = LinearRegression::of(Dataset::of([[0, 1], [1, 0]]));
+        $r = LinearRegression::of(Dataset::of([1, 0]));
         $this->assertSame(1, $r->intercept()->value());
     }
 
     public function testSlope()
     {
-        $r = LinearRegression::of(Dataset::of([[0, 0], [1, 1]]));
+        $r = LinearRegression::of(Dataset::of([0, 1]));
         $this->assertInstanceOf(Number::class, $r->slope());
         $this->assertSame($r->slope(), $r->slope());
         $this->assertSame(1, $r->slope()->value());
 
-        $r = LinearRegression::of(Dataset::of([[0, 1], [1, 0]]));
+        $r = LinearRegression::of(Dataset::of([1, 0]));
         $this->assertSame(-1, $r->slope()->value());
     }
 
     public function testComplexCase()
     {
-        $r = LinearRegression::of(Dataset::of([
-            [0, 0.5],
-            [1, 1],
-            [2, 4],
-            [3, -1],
-        ]));
+        $r = LinearRegression::of(Dataset::of([0.5, 1, 4, -1]));
 
         $this->assertSame(-0.15, $r->slope()->value());
         $this->assertSame(1.35, $r->intercept()->value());
@@ -52,12 +47,7 @@ class LinearRegressionTest extends TestCase
 
     public function testRootMeanSquareDeviation()
     {
-        $regression = LinearRegression::of(Dataset::of([
-            [0, 0.5],
-            [1, 1],
-            [2, 4],
-            [3, -1],
-        ]));
+        $regression = LinearRegression::of(Dataset::of([0.5, 1, 4, -1]));
 
         $this->assertInstanceOf(
             Number::class,
