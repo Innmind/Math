8bc1b6b4e3bb88488436dc61ac42303c7dcf592c
Baptiste Langlade <baptouuuu@gmail.com>
2022-09-25 10:50:05 +0200

    do not pre-compute a quantile

--- src/Quantile/Quantile.php
+++ src/Quantile/Quantile.php
@@ -28,27 +28,15 @@ use Innmind\Immutable\{
  */
 final class Quantile
 {
-    private Quartile $min;
-    private Quartile $max;
-    private Mean $mean;
-    private Quartile $median;
-    private Quartile $firstQuartile;
-    private Quartile $thirdQuartile;
+    /** @var Sequence<Number> */
+    private Sequence $values;
 
     /**
      * @param Sequence<Number> $values
      */
     private function __construct(Sequence $values)
     {
-        $values = $values->sort(asc(...));
-
-        // todo do not pre-compute the values
-        $this->min = $this->buildMin($values);
-        $this->max = $this->buildMax($values);
-        $this->mean = $this->buildMean($values);
-        $this->median = $this->buildMedian($values);
-        $this->firstQuartile = $this->buildFirstQuartile($values);
-        $this->thirdQuartile = $this->buildThirdQuartile($values);
+        $this->values = $values->sort(asc(...));
     }
 
     /**
@@ -61,28 +49,25 @@ final class Quantile
         return new self($values);
     }
 
-    /**
-     * Return the minimum value
-     */
     public function min(): Quartile
     {
-        return $this->min;
+        return $this->values->first()->match(
+            Quartile::of(...),
+            static fn() => throw new LogicException('Empty dataset'),
+        );
     }
 
-    /**
-     * Return the maximum value
-     */
     public function max(): Quartile
     {
-        return $this->max;
+        return $this->values->last()->match(
+            Quartile::of(...),
+            static fn() => throw new LogicException('Empty dataset'),
+        );
     }
 
-    /**
-     * Return the mean value
-     */
     public function mean(): Number
     {
-        return $this->mean;
+        return Mean::of(...$this->values->toList());
     }
 
     /**
@@ -90,119 +75,43 @@ final class Quantile
      */
     public function median(): Quartile
     {
-        return $this->median;
+        return Quartile::of(Median::of(...$this->values->toList()));
     }
 
     public function firstQuartile(): Quartile
     {
-        return $this->firstQuartile;
+        return Quartile::of($this->buildQuartile(Real::of(0.25)));
     }
 
     public function thirdQuartile(): Quartile
     {
-        return $this->thirdQuartile;
-    }
-
-    /**
-     * Extract the minimum value from the dataset
-     *
-     * @param Sequence<Number> $values
-     */
-    private function buildMin(Sequence $values): Quartile
-    {
-        return $values->first()->match(
-            Quartile::of(...),
-            static fn() => throw new LogicException('Empty dataset'),
-        );
-    }
-
-    /**
-     * Extract the maximum value from the dataset
-     *
-     * @param Sequence<Number> $values
-     */
-    private function buildMax(Sequence $values): Quartile
-    {
-        return $values->last()->match(
-            Quartile::of(...),
-            static fn() => throw new LogicException('Empty dataset'),
-        );
-    }
-
-    /**
-     * Build the mean value from the dataset
-     *
-     * @param Sequence<Number> $values
-     */
-    private function buildMean(Sequence $values): Mean
-    {
-        return Mean::of(...$values->toList());
-    }
-
-    /**
-     * Extract the median from the dataset
-     *
-     * @param Sequence<Number> $values
-     */
-    private function buildMedian(Sequence $values): Quartile
-    {
-        return Quartile::of(Median::of(...$values->toList()));
-    }
-
-    /**
-     * Extract the first quartile
-     *
-     * @param Sequence<Number> $values
-     */
-    private function buildFirstQuartile(Sequence $values): Quartile
-    {
-        return Quartile::of($this->buildQuartile(
-            Real::of(0.25),
-            $values,
-        ));
-    }
-
-    /**
-     * Extract the third quartile
-     *
-     * @param Sequence<Number> $values
-     */
-    private function buildThirdQuartile(Sequence $values): Quartile
-    {
-        return Quartile::of($this->buildQuartile(
-            Real::of(0.75),
-            $values,
-        ));
+        return Quartile::of($this->buildQuartile(Real::of(0.75)));
     }
 
     /**
      * Return the value describing the quartile at the given percentage
-     *
-     * @param Sequence<Number> $values
      */
-    private function buildQuartile(
-        Number $percentage,
-        Sequence $values,
-    ): Number {
-        $index = (int) Integer::of($values->size())
+    private function buildQuartile(Number $percentage): Number
+    {
+        $index = (int) Integer::of($this->values->size())
             ->multiplyBy($percentage)
             ->roundUp()
             ->collapse()
             ->value();
 
-        return $values->match(
-            static fn($first, $rest) => $rest->match(
-                static fn($second, $rest) => match ($rest->empty()) {
+        return $this->values->match(
+            fn($first, $rest) => $rest->match(
+                fn($second, $rest) => match ($rest->empty()) {
                     true => $first->add($second)->divideBy(Value::two),
                     false => Maybe::all(
-                        $values->get($index),
-                        $values->get($index - 1),
+                        $this->values->get($index),
+                        $this->values->get($index - 1),
                     )
                         ->map(Addition::of(...))
                         ->map(static fn($sum) => $sum->divideBy(Value::two))
                         ->match(
                             static fn($quartile) => $quartile,
-                            static fn() => throw new LogicException("Operation not working for size {$values->size()}"),
+                            fn() => throw new LogicException("Operation not working for size {$this->values->size()}"),
                         ),
                 },
                 static fn() => $first,
